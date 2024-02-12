181ab5c23dc589b6f8e67dd57558b33e018b89cd
Ved Jha <vedprakash@fanclash.in>
2024-02-12 18:49:31 +0530

    basic singleton class

---
+++ singleton/basicsingle.py
@@ -0,0 +1,9 @@
+class Singleton:
+    _instance=None
+
+    def __new__(cls):
+        # In this example, the __new__ method is overridden to ensure that only one instance of the class is created. If the instance does not exist, a new one is created, otherwise, the existing instance is returned.
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+        
