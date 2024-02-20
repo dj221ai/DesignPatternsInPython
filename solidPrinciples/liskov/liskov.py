5ae58d379ce3207cf3502be3a30d1a193d6120f6
Ved Jha <vedprakash@fanclash.in>
2024-02-20 18:49:10 +0530

    liskov principle

---
+++ solidPrinciples/liskov/liskov.py
@@ -0,0 +1,38 @@
+'''FlyingBird and NonFlyingBird both inherit bird class but both  provide alternative implementation of fly method and ostric and parrot inherit from respective classes which satiesfies LSP any subclasses of bird can now be substituted without altering the correctness of a program.'''
+
+class Bird:
+    def fly(self):
+        pass
+
+
+class FlyingBird(Bird):
+    def fly(self):
+        print("I can Flyyyy!!")
+
+
+class NonFlyingBird(Bird):
+    def fly(self):
+        print("I can't Fly!! ")
+
+
+
+class Ostrich(NonFlyingBird):
+    pass
+    # def fly(self):
+    #     return super().fly()
+
+
+class Parrot(FlyingBird):
+    pass
+    
+
+
+ostobj=Ostrich()
+ostobj.fly()
+
+pobj=Parrot()
+pobj.fly()
+
+
+
+
